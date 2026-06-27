418272cf30fdd493710647281fba0259cd39d21c
st93642 <st93642@students.tsi.lv>
2026-06-27 06:34:51 +0300

    comm exam

---
+++ Exam/sketch.ino
@@ -0,0 +1,11 @@
+/*
+
+  Final Exam Practical Task - CSS_VP
+  Arduino sketch entry point for AVR assembly project.
+
+  Student: Igors Oleinikovs
+  Student code: 93642
+
+  The implementation is in asm.S.
+
+*/
